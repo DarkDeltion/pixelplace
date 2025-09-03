a1aab188de5391f3f92ebe3a86fdc53ecec9348e
damien <97094049@st.deltion.nl>
2025-09-03 11:48:11 +0200

    PixelPlace Initiated.
    project has started

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- navbar -->
+  <nav>
+    <div class="nv-underglow">
+      <div class="nav-container">
+        <div class="nav-button">
+          <a href="#">Home</a>
+        </div>
+
+        <div class="title">
+          <h1>PixelPlace</h1>
+        </div>
+
+        <div class="nav-button">
+          <a href="#">Login</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <!-- main content -->
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  height: 60px;
+  background-color: #1e1e1e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Pixelify Sans', sans-serif;
+}
+</style>
+
+<script setup lang="ts"></script>
